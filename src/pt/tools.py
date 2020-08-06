e33b3c26f4489c62dbfdaef81661ada46894b43d
Dmitry <cher-di@mail.ru>
2020-08-07 01:31:53 +0500

    Fixed bug in call_grader when running on Windows

--- src/pt/tools.py
+++ src/pt/tools.py
@@ -51,11 +51,14 @@ def call_grader(filepath: str, password: str, attempts=10, delay=500, host='loca
             raise GraderError(completed_process.stderr)
 
     for line in completed_process.stdout.split(_os.linesep):
-        try:
-            data = _json.loads(line)
-            return ActivityFileData(**data)
-        except _json.JSONDecodeError:
-            pass
+        first_bracket_index = line.find('{')
+        last_bracket_index = line.rfind('}')
+        if first_bracket_index != -1 and last_bracket_index != -1:
+            try:
+                data = _json.loads(line[first_bracket_index:last_bracket_index + 1])
+                return ActivityFileData(**data)
+            except _json.JSONDecodeError:
+                pass
 
 
 def call_meta(xml_filepath: str, pta_filepath: str):
