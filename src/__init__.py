5a7e29c0ef55170de9c9bd6bb33040f20306b13f
Dmitry <cher-di@mail.ru>
2020-08-07 23:42:15 +0500

    Add RESOURCE_ROOT

--- src/__init__.py
+++ src/__init__.py
@@ -4,3 +4,4 @@ import os as _os
 SOURCE_ROOT = _os.path.dirname(_os.path.realpath(__file__))
 PROJECT_ROOT = _os.path.dirname(SOURCE_ROOT)
 TOOLS_ROOT = _os.path.join(PROJECT_ROOT, 'tools')
+RESOURCES_ROOT = _os.path.join(PROJECT_ROOT, 'res')
