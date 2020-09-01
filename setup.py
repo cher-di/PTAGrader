24b353d8c7b185f75b68757690502b71c4721498
Dmitry <cher-di@mail.ru>
2020-09-01 14:53:55 +0300

    Added tools directory making to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ import os
 import shutil
 
 from src import CONFIG_ROOT, RESOURCES_ROOT
+from src.pt.path import PT_GRADER_ROOT
 
 CONFIG_TEMPLATES_ROOT = os.path.join(RESOURCES_ROOT, 'config')
 
 if __name__ == '__main__':
     # copy configs from templates
     shutil.copytree(CONFIG_TEMPLATES_ROOT, CONFIG_ROOT, dirs_exist_ok=True)
+
+    # make directories for tools
+    os.makedirs(PT_GRADER_ROOT, exist_ok=True)
