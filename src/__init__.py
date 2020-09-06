34fd3f5803ccb5c119bcd76490b3d967cd50d0df
Dmitry <cher-di@mail.ru>
2020-09-06 18:18:44 +0300

    Added FILES_ROOT

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 import os
 
-
 SOURCE_ROOT = os.path.dirname(os.path.realpath(__file__))
 PROJECT_ROOT = os.path.dirname(SOURCE_ROOT)
 RESOURCES_ROOT = os.path.join(PROJECT_ROOT, 'res')
 CONFIG_ROOT = os.path.join(PROJECT_ROOT, 'config')
+FILES_ROOT = os.path.join(PROJECT_ROOT, 'files')
