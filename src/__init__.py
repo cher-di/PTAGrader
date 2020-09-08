5aca376afab3b0dcf5946bd9605db53d02f2ca2e
Dmitry <cher-di@mail.ru>
2020-09-08 23:53:28 +0300

    Changed global project paths

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,12 @@
 import os
 
+from pathlib import Path
+
 SOURCE_ROOT = os.path.dirname(os.path.realpath(__file__))
 PROJECT_ROOT = os.path.dirname(SOURCE_ROOT)
 RESOURCES_ROOT = os.path.join(PROJECT_ROOT, 'res')
-CONFIG_ROOT = os.path.join(PROJECT_ROOT, 'config')
-FILES_ROOT = os.path.join(PROJECT_ROOT, 'files')
+
+HOME = str(Path().home())
+DATA_ROOT = os.path.join(HOME, '.PTAGrader')
+CONFIG_ROOT = os.path.join(DATA_ROOT, 'config')
+FILES_ROOT = os.path.join(DATA_ROOT, 'files')
